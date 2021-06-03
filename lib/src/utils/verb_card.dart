50797ce36b024205dd6c703cee6206995e9618d4
tincho77 <rosas.martin.exequiel@gmail.com>
2021-06-03 17:13:27 -0300

    verdCard() dbHelper

--- lib/src/utils/verb_card.dart
+++ lib/src/utils/verb_card.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:verb_selection/src/utils/DB/initialize_DB.dart';
 
+final dbHelper = DatabaseHelper.instance;
+int uses;
 Widget verbCard() {
+  dbHelper.queryMostUsedVerbs();
+  dbHelper.queryAllPronouns();
   return Center(
       child: Column(
     mainAxisAlignment: MainAxisAlignment.center,
     children: <Widget>[
-      Container(
-        child: TextButton(onPressed: () {}, child: Text('Verbos Irregulares')),
-      )
+      Container(child: Text('hola')),
     ],
   ));
 }
