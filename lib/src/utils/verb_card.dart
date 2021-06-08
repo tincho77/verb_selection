004a2f3d369ad75691803b3d0f0f0bacebc36d75
martinleandrodiaz <martinleodiaz@gmail.com>
2021-06-08 12:17:59 -0300

    Code update. Added card with buttons

--- lib/src/utils/verb_card.dart
+++ lib/src/utils/verb_card.dart
@@ -17,108 +17,98 @@ Widget verbCard(BuildContext context) {
   dbHelper.queryAllPronouns();*/
   //dbHelper.queryMostUsedVerbs1();
 
- return SafeArea(
-     child: Container(
-       child:
-       new FutureBuilder<dynamic>(
-       future: getMostUsedVerbs(),
-       builder: (context, snapshot) {
-         if (snapshot.hasData) {
-           return ListView.builder(
-             padding: EdgeInsets.only(top: 5, bottom: 16),
-              //scrollDirection: Axis.horizontal,
-               itemCount: snapshot.data.length,
-               itemBuilder: (context, index){
-                 return Column(
-                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                     crossAxisAlignment: CrossAxisAlignment.center,
-                     children: <Widget>[
-
-                       new Container(
-
-                         /*height: heightContainer,
-                     width: SizeConfig.safeBlockHorizontal * 100,*/
-                         padding: EdgeInsets.only(
-                             right: 10, left: 10, bottom: 10),
-                         margin: const EdgeInsets.only(left: 10.0, right: 10.0,),
-                         //    padding: EdgeInsets.only(top: 2, right: 10, bottom: 10, left: 10),
-                         //height: 150,
-                         decoration: BoxDecoration(
-                             color: Colors.white70,
-                             borderRadius: BorderRadius.circular(12),
-                             boxShadow: [
-                               BoxShadow(
-                                 color: Colors.grey,
-                                 offset: Offset(1, 1),
-                                 blurRadius: 5,
-                               ),
-                             ]),
-
-                         child: Center(
-                           child: Column(
-                             children: <Widget>[
-                               SizedBox(height: 10),
-                               Row(
-                                 mainAxisAlignment: MainAxisAlignment
-                                     .spaceBetween,
-                                 mainAxisSize: MainAxisSize.min,
-                                 verticalDirection: VerticalDirection.down,
-
-                                 children: <Widget>[
-                                   VerticalDivider(
-                                     width: 5,
-                                   ),
-
-                                   ElevatedButton(
-                                     style: ElevatedButton.styleFrom(
-                                       padding: EdgeInsets.only(top: 12,
-                                           bottom: 12,
-                                           right: 26,
-                                           left: 28),
-                                       elevation: 5,
-                                       //primary: col,
-                                       onPrimary: Colors.white,
-                                       shape: RoundedRectangleBorder(
-                                           borderRadius: BorderRadius.circular(
-                                               30)),
-                                       alignment: Alignment.center,
-                                     ),
-                                     child: new Text(
-                                       snapshot.data[index]['description'].toString(),
-                                       textAlign: TextAlign.center,
-                                       style: TextStyle(
-                                         color: Colors.white,
-                                         fontSize: 15,
-                                       ),
-                                     ),
-                                     onPressed: () {},
-
-                                   ),
-                                 ],
-                               ),
-                             ],
-                           ),
-                         ),
-                       ),
-                     ]
-                 );
-               }
-             //children: <Widget>[
-
-             //],
-
-           );
-         }
-         else if (snapshot.hasError) {
-           return new Text("${snapshot.error}");
-         }
-         return new Container(
-           alignment: AlignmentDirectional.center,
-           child: new CircularProgressIndicator(),
-         );
-       }
-     ),
-  ),
- );
+  return SafeArea(
+    child: Container(
+      child: new FutureBuilder<dynamic>(
+          future: getMostUsedVerbs(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Center(
+                  child: Card(
+                color: Colors.white70,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                margin: const EdgeInsets.only(left: 10.0, right: 10.0),
+                child: Container(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        verticalDirection: VerticalDirection.down,
+                        children: <Widget>[
+                      GridView.count(
+                          shrinkWrap: true,
+                          crossAxisCount: 3,
+                          padding: EdgeInsets.only(top: 25.0),
+                          childAspectRatio: 3 / 2,
+                          children:
+                              List.generate(snapshot.data.length, (index) {
+                            if (snapshot.data[index]['description']
+                                    .toString() !=
+                                null) {
+                              return Container(
+                                  child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                verticalDirection: VerticalDirection.down,
+                                children: <Widget>[
+                                  VerticalDivider(
+                                    width: 5,
+                                    color: Colors.white12,
+                                  ),
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      padding: EdgeInsets.only(
+                                          top: 12,
+                                          bottom: 12,
+                                          right: 18,
+                                          left: 22),
+                                      elevation: 5,
+                                      //primary: col,
+                                      onPrimary: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30)),
+                                      alignment: Alignment.center,
+                                    ),
+                                    child: new Text(
+                                      snapshot.data[index]['description']
+                                          .toString(),
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ],
+                              ));
+                            } else {
+                              return Container(
+                                //color: Colors.blue,
+                                margin: EdgeInsets.all(10.0),
+                              );
+                            }
+                          })),
+                      Align(
+                        alignment: Alignment(0.8, -1.0),
+                        heightFactor: 0.5,
+                        child: FloatingActionButton(
+                          onPressed: null,
+                          child: Icon(Icons.add),
+                        ),
+                      )
+                    ])),
+              ));
+            } else if (snapshot.hasError) {
+              return new Text("${snapshot.error}");
+            }
+            return new Container(
+              alignment: AlignmentDirectional.center,
+              child: new CircularProgressIndicator(),
+            );
+          }),
+    ),
+  );
 }
-  
